d29880d6c0f17a271f248b623150305f8c5e173b
Vinayak Rodd <150567680+vinayakRodd@users.noreply.github.com>
2025-02-20 21:49:57 +0530

    Update resetPassword.controllers.js

--- resetPassword.controllers.js
+++ resetPassword.controllers.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
-const User = require('../models/user.models');
+const User = require('./user.models');
 
 const router = express.Router();
 
