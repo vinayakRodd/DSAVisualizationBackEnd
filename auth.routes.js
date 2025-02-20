4ce54eb7e6e658ecb994c9764be37b1d493254f0
Vinayak Rodd <150567680+vinayakRodd@users.noreply.github.com>
2025-02-20 21:48:02 +0530

    Update auth.routes.js

--- auth.routes.js
+++ auth.routes.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { signup, login, logout } = require("../controllers/auth.controller");
-const forgotPassword = require("../controllers/forgotPassword.controller");
-const VerifyOtp = require('../controllers/verifyOtp')
-const ResetPassword = require('../controllers/resetPassword.controllers')
-const gmailVerify = require('../controllers/verifyGmail.controller');
+const { signup, login, logout } = require("./auth.controller");
+const forgotPassword = require("./forgotPassword.controller");
+const VerifyOtp = require('./verifyOtp')
+const ResetPassword = require('./resetPassword.controllers')
+const gmailVerify = require('./verifyGmail.controller');
 const router = express.Router();
 
 router.post("/signup", signup);
